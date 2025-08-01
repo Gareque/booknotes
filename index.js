01f81690cbb27bb19505513850f63cd5b47a5034
Gareque <grigeral@gmail.com>
2025-08-01 18:40:13 +0100

    Performance improvements

--- index.js
+++ index.js
@@ -34,35 +34,12 @@ const sortHandler = async (req, res) => {
   const { field } = req.params;
 
   if (!validFields.includes(field)) {
-    return res
-      .status(400)
-      .send("Invalid sort field, please enter a valid option");
+    return res.status(400).send("Invalid sort field");
   }
-
-
 
   try {
     const result = await db.query(`SELECT * FROM books ORDER BY ${field} ASC`);
-    const books = result.rows;
-
-    // Add the book cover via API call to Open Library Cover
-    for (const book of books) {
-        const isbn = book.isbn;
-        const url = `https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`;
-
-        try {
-          const response = await axios.head(url); // This will check the cover exists before processing
-          if (response.status === 200) {
-            // 200 confirms the image is found and so can proceed
-            book.coverUrl = url;
-          } else {
-            book.coverUrl = null;
-          }
-        } catch (err) {
-          book.coverUrl = null;
-        }
-      }
-
+    const books = result.rows; // Just the DB data, no cover URLs
     res.render("index.ejs", { books });
   } catch (err) {
     console.error(err);
